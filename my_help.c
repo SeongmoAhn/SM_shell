69c69584fdc9c44bb1b6517856ecb60c9459c9b1
SeongmoAhn <asm0619@naver.com>
2023-08-11 20:56:59 +0900

    help 파일 ls 명령어 수정

--- my_help.c
+++ my_help.c
@@ -2,9 +2,13 @@
 
 void help() {
     printf("Usage : \n");
-    printf(" > help\n");
-    printf(" > exit\n");
-    printf(" > clear\n");
-    printf(" > pwd\n");
-    printf(" > ls\n\n");
+    printf("  > help\n");
+    printf("  > exit\n");
+    printf("  > clear\n");
+    printf("  > pwd\n");
+    printf("  > ls [OPTION]\n");
+    printf("    -a : include directory entries whose names begin with a '.'\n");
+    printf("    -l : print directory entries by list format\n");
+
+    printf("\n");
 }
